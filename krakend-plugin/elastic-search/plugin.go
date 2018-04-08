adcb0da6afdc2f79eec627dc41712c8dce41385c
kpacha <kpacha666@gmail.com>
2018-04-08 19:53:08 +0200

    plugins updated

--- krakend-plugin/elastic-search/plugin.go
+++ krakend-plugin/elastic-search/plugin.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/devopsfaith/krakend-martian/register"
 	"github.com/kpacha/martian-components/body/elastic-search/modifier"
 )
 
+var Registrable registrable
+
+type registrable int
+
+func (r *registrable) RegisterExternal(setter func(namespace, name string, v interface{})) error {
+	fmt.Println("registrable", r, "from plugin 'krakend_martian-ES' is registering its components depending on external modules at", register.Namespace)
+
+	component := register.NewComponent([]string{register.ScopeRequest},
+		func(b []byte) (interface{}, error) {
+			return modifier.FromJSON(b)
+		})
+	setter(register.Namespace, "body.ESQuery", component)
+
+	return nil
+}
+
 func init() {
-	register.Set("body.ESQuery", []register.Scope{register.ScopeRequest}, func(b []byte) (interface{}, error) {
-		return modifier.FromJSON(b)
-	})
+	fmt.Println("plugin 'krakend_martian-ES' loaded!")
 }
 
 func main() {
--- krakend-plugin/json-schema/plugin.go
+++ krakend-plugin/json-schema/plugin.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/devopsfaith/krakend-martian/register"
 	"github.com/kpacha/martian-components/body/json-schema/verifier"
 )
 
+var Registrable registrable
+
+type registrable int
+
+func (r *registrable) RegisterExternal(setter func(namespace, name string, v interface{})) error {
+	fmt.Println("registrable", r, "from plugin 'krakend_martian-JSON' is registering its components depending on external modules at", register.Namespace)
+
+	requestComponent := register.NewComponent([]string{register.ScopeRequest},
+		func(b []byte) (interface{}, error) {
+			return verifier.RequestVerifierFromJSON(b)
+		})
+	setter(register.Namespace, "body.JSON-SCHEMA.Request", requestComponent)
+
+	responseComponent := register.NewComponent([]string{register.ScopeResponse},
+		func(b []byte) (interface{}, error) {
+			return verifier.ResponseVerifierFromJSON(b)
+		})
+	setter(register.Namespace, "body.JSON-SCHEMA.Response", responseComponent)
+
+	return nil
+}
+
 func init() {
-	register.Set("body.JSON-SCHEMA.Request", []register.Scope{register.ScopeRequest}, func(b []byte) (interface{}, error) {
-		return verifier.RequestVerifierFromJSON(b)
-	})
-	register.Set("body.JSON-SCHEMA.Response", []register.Scope{register.ScopeResponse}, func(b []byte) (interface{}, error) {
-		return verifier.ResponseVerifierFromJSON(b)
-	})
+	fmt.Println("plugin 'krakend_martian-JSON' loaded!")
 }
 
 func main() {
